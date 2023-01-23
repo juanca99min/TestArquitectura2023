12d283463bbeb95930d57eac0a599cda8f07c58c
Juan Carlos Ortiz <juanortizmg@gmail.com>
2023-01-23 13:29:44 -0500

    Adding config for app center

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -4,10 +4,10 @@ BUILD_SRC_ROOT=$APPCENTER_SOURCE_DIRECTORY
 KEYSTORE_FILENAME="99minutos"
 
 # Create .certs directory under the source root if doesn't exist
-[ -d "$BUILD_SRC_ROOT/.certs" ] || mkdir -p "$BUILD_SRC_ROOT/app/.certs"
+[ -d "$BUILD_SRC_ROOT/.certs" ] || mkdir -p "$BUILD_SRC_ROOT/.certs"
 
 echo "Este es un mensaje de prueba"
 
 echo "$BUILD_SRC_ROOT/.certs"
 # Decode the base64-encoded certificate contexts to the files
-echo "$JKS_BASE_64" | base64 --decode > "$BUILD_SRC_ROOT/app/.certs/$KEYSTORE_FILENAME.jks"
+echo "$JKS_BASE_64" | base64 --decode > "$BUILD_SRC_ROOT/.certs/$KEYSTORE_FILENAME.jks"
