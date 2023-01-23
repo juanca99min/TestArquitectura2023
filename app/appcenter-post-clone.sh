afe0f4015b53ba011c606672bf6192ecaca7ac1a
Juan Carlos Ortiz <juanortizmg@gmail.com>
2023-01-23 12:50:17 -0500

    Clean gradle files

--- app/appcenter-post-clone.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#mkdir -p ./build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
-#mkdir -p ./build/outputs/bundle/; mv build/app/outputs/bundle/release/app.aab $_
---
+++ appcenter-post-clone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BUILD_SRC_ROOT=$APPCENTER_SOURCE_DIRECTORY
+KEYSTORE_FILENAME="99minutos"
+
+# Create .certs directory under the source root if doesn't exist
+[ -d "$BUILD_SRC_ROOT/.certs" ] || mkdir -p "$BUILD_SRC_ROOT/.certs"
+
+# Decode the base64-encoded certificate contexts to the files
+echo "$JKS_BASE_64" | base64 --decode > "$BUILD_SRC_ROOT/.certs/$KEYSTORE_FILENAME.jks"
